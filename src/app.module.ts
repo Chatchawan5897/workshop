b91bcc351a4dafe44ab27bd8c17f882c3680aedf
Chatchawan pasuriwong <Chatchawan.pasu@gmail.com>
2024-10-30 11:42:40 +0700

    update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,22 +8,34 @@ import { SubdistrictModule } from './subdistrict/subdistrict.module';
 import { Province } from './province/entities/province.entity';
 import { District } from './district/entities/district.entity';
 import { Subdistrict } from './subdistrict/entities/subdistrict.entity';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),// ทำให้สามารถเข้าถึงค่าจาก .env
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'workshop',
-      entities: [
-        Province,District ,Subdistrict
-      ],
+      type:'postgres',
+      host:process.env.DB_HOST,
+      port:+process.env.DB_POST,
+      username:process.env.DB_USER,
+      password:process.env.DB_PASSWORD,
+      database:process.env.DB_NAME,
+      autoLoadEntities: true,
       synchronize: true,
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '',
+    //   database: 'workshop',
+    //   entities: [
+    //     Province,District ,Subdistrict
+    //   ],
+    //   synchronize: true,
+    // }),
     ProvinceModule, DistrictModule, SubdistrictModule],
   controllers: [AppController],
   providers: [AppService],
